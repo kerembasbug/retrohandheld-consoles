bd029a3df54daea8379121f5f4d9a04b98f39bc9
kerembasbug <kerembasbug@gmail.com>
2025-11-28 14:34:29 +0300

    Clean up: Remove Zeabur and GitHub deployment scripts

--- zeabur-env-commands.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Zeabur Environment Variables
-zeabur env add AMAZON_ACCESS_KEY=AKPADGRLO31764065866
-zeabur env add AMAZON_SECRET_KEY=/JcoCfU6YsxnCV6WwRb0nIZhPN71kd7kscnFPUIl
-zeabur env add AMAZON_PARTNER_TAG=your-tag-20
